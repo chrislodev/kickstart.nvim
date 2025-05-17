4729f22482cbd0fd9d0fa2d8c31ade0045f45340
Chris Lo <hello@chrislo.com.au>
2025-05-17 15:09:02 +1000

    Added augment

---
+++ lua/plugins/augment.lua
@@ -0,0 +1,35 @@
+return {
+  'augmentcode/augment.vim',
+  keys = {
+    {
+      '<leader>at',
+      '<cmd>Augment chat-toggle<cr>',
+      desc = 'Toggle chat',
+      mode = { 'n', 'i' },
+    },
+    {
+      '<leader>as',
+      '<cmd>Augment signin<cr>',
+      desc = 'Sign in',
+      mode = { 'n', 'i' },
+    },
+    {
+      '<leader>aS',
+      '<cmd>Augment status<cr>',
+      desc = 'Status',
+      mode = { 'n', 'i' },
+    },
+    {
+      '<leader>ac',
+      '<cmd>Augment chat<cr>',
+      desc = 'Chat',
+      mode = { 'n', 'i' },
+    },
+    {
+      '<leader>an',
+      '<cmd>Augment chat-new<cr>',
+      desc = 'New chat',
+      mode = { 'n', 'i' },
+    },
+  },
+}
