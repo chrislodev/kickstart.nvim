3fd9d7b82313fd6eaad2d576cb7b3980faabfc98
Chris Lo <hello@chrislo.com.au>
2025-05-17 14:55:46 +1000

    Removed unused keybinds

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -224,20 +224,6 @@ return {
         end,
         desc = 'Widgets',
       },
-      -- {
-      --   '<leader>dO',
-      --   function()
-      --     require('dap').step_out()
-      --   end,
-      --   desc = 'Step Out',
-      -- },
-      -- {
-      --   '<leader>do',
-      --   function()
-      --     require('dap').step_over()
-      --   end,
-      --   desc = 'Step Over',
-      -- },
       {
         '<leader>da',
         function()
