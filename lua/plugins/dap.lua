d990aa16b90827577d3aeac9aaf8be4d2d49afdb
Chris Lo <hello@chrislo.com.au>
2025-05-17 14:11:51 +1000

    dap UI now opening automatically

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -263,6 +263,9 @@ return {
         version = '1.*',
       },
       {
+        'rcarriga/nvim-dap-ui',
+      },
+      {
         'mxsdev/nvim-dap-vscode-js',
         config = function()
           ---@diagnostic disable-next-line: missing-fields
@@ -302,5 +305,29 @@ return {
         build = './install.sh',
       },
     },
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'nvim-neotest/nvim-nio' },
+    -- stylua: ignore
+    keys = {
+      { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+      { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+    },
+    opts = {},
+    config = function(_, opts)
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open {}
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close {}
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close {}
+      end
+    end,
   },
 }
