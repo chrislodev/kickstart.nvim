d2ff8bfa82bcb1f7a9bb1d95888479f59a5067d7
Chris Lo <hello@chrislo.com.au>
2025-05-19 07:54:47 +1000

    Add marks.nvim
    Displays gutter icons for marks

---
+++ lua/plugins/marks.lua
@@ -0,0 +1,17 @@
+
+return {
+  "chentoast/marks.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("marks").setup({
+      -- LazyVim-like configuration
+      default_mappings = true,
+      builtin_marks = { ".", "<", ">", "^" },
+      cyclic = true,
+      force_write_shada = false,
+      refresh_interval = 250,
+      sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+      excluded_filetypes = {},
+    })
+  end
+}
