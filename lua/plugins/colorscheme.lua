92da40794fcd1f51dc1e112c0d69bedaea9c469e
Chris Lo <hello@chrislo.com.au>
2025-05-18 11:15:40 +1000

    Change colour scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,14 +9,38 @@
 --     ---@diagnostic disable-next-line: missing-fields
 --     require('tokyonight').setup {
 --       styles = {
---         comments = { italic = false }, -- Disable italics in comments
+--         comments = { italic = true }, -- Enable/Disable italics in comments
 --       },
 --     }
---
 --     -- Load the colorscheme here.
 --     -- Like many other themes, this one has different styles, and you could load
 --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
 --     vim.cmd.colorscheme 'tokyonight-night'
+--   end,
+-- }
+
+-- return {
+--   'oxfist/night-owl.nvim',
+--   lazy = false, -- set to false to make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function(_, opts)
+--     local night_owl = require 'night-owl'
+--     night_owl.setup {
+--       bold = true,
+--       italics = true,
+--       underline = true,
+--       undercurl = true,
+--       transparent_background = true,
+--     }
+--     -- TODO:Setting the background colour of the notify popup no longer works since Lazyvim changed
+--     -- to use `Snacks` instead of `notify`. I have to figure out how to change the background.
+--     -- require("notify").setup(vim.tbl_extend("keep", {
+--     --   background_colour = "#f4c284",
+--     -- }, opts))
+--     -- vim.cmd.colorscheme("night-owl")
+--     -- Change the background colour of the currently selected line
+--     vim.cmd [[highlight CursorLine guibg=#dddddd]]
+--     vim.cmd.colorscheme 'night-owl'
 --   end,
 -- }
 
