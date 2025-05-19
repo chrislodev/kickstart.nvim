92d2c98d52783910cbd2787e2fe66850f47537c0
Chris Lo <hello@chrislo.com.au>
2025-05-20 07:58:38 +1000

    Set transparency for night-owl

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -50,5 +50,6 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Moves lines up in visual 
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Move down in buffer with cursor centered' })
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Move up in buffer with cursor centered' })
 
+-- Keeps cursor centered when searching
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
