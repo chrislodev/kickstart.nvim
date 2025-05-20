a4369944229da02674f0f55aa921dc0ee9f58962
Chris Lo <hello@chrislo.com.au>
2025-05-21 07:53:12 +1000

    Add keymaps to search for todos

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,2 +1,30 @@
 return -- Highlight todo, notes, etc in comments
-{ 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
+{
+  'folke/todo-comments.nvim',
+  event = {
+    'BufReadPre',
+    'BufNewFile',
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  opts = {
+    signs = false,
+  },
+  keys = {
+    {
+      '<leader>st',
+      function()
+        require('snacks').picker.todo_comments()
+      end,
+      desc = 'Todo',
+    },
+    {
+      '<leader>sT',
+      function()
+        require('snacks').picker.todo_comments { keywords = { 'TODO', 'FIX', 'FIXME' } }
+      end,
+      desc = 'Todo/Fix/Fixme',
+    },
+  },
+}
