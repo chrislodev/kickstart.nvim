c4b0d3627a0e947e895fc0805bd78e60cbad5b2e
Chris Lo <hello@chrislo.com.au>
2025-05-22 06:51:25 +1000

    Add bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,16 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  opts = {
+    options = {
+      truncate_names = true,
+      numbers = 'none',
+      indicator = {
+        icon = '▎', -- this should be omitted if indicator style is not 'icon'
+        -- style = 'icon' | 'underline' | 'none',
+        style = 'underline',
+      },
+    },
+  },
+}
