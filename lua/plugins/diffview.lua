1a1ca23a009281779689ced8e98f104780a83653
Chris Lo <hello@chrislo.com.au>
2025-05-31 13:24:31 +1000

    Add diffview

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,15 @@
+return {
+  'sindrets/diffview.nvim',
+  keys = {
+    {
+      '<leader>gdo',
+      '<cmd>DiffviewOpen<cr>',
+      desc = 'Open diffview',
+    },
+    {
+      '<leader>gdc',
+      '<cmd>DiffviewClose<cr>',
+      desc = 'Close diffview',
+    },
+  },
+}
