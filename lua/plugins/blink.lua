63147fb58d0d2eb3a4d24bfbfb703ca62e4b0605
Chris Lo <hello@chrislo.com.au>
2025-05-31 13:23:07 +1000

    Blink updates
    Use tab for completion accept.
    Auto show documentation.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -40,7 +40,7 @@ return {
       --   <c-y> to accept ([y]es) the completion.
       --    This will auto-import if your LSP supports it.
       --    This will expand snippets if the LSP sent a snippet.
-      -- 'super-tab' for tab to accept
+      -- 'super-tab' --for tab to accept
       -- 'enter' for enter to accept
       -- 'none' for no mappings
       --
@@ -58,6 +58,7 @@ return {
       --
       -- See :h blink-cmp-config-keymap for defining your own keymap
       preset = 'default',
+      -- preset = 'super-tab',
 
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -72,7 +73,8 @@ return {
     completion = {
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
-      documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      ghost_text = { enabled = false },
     },
 
     sources = {
