8af0fe985e53e35caeda8599a0b84cded538caa9
Chris Lo <hello@chrislo.com.au>
2025-05-31 13:28:04 +1000

    conform - change conflicting keymap

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,7 +5,7 @@ return {
   cmd = { 'ConformInfo' },
   keys = {
     {
-      '<leader>f',
+      '<leader>cf',
       function()
         require('conform').format { async = true, lsp_format = 'fallback' }
       end,
@@ -37,6 +37,7 @@ return {
       -- You can use 'stop_after_first' to run the first available formatter from the list
       javascript = { 'prettierd', 'prettier', stop_after_first = true },
       typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      html = { 'prettierd', 'prettier', stop_after_first = true },
     },
   },
 }
