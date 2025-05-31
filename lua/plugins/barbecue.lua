8ab1b5a1e3c93b912afb41e1f86c6bd88b71c692
Chris Lo <hello@chrislo.com.au>
2025-05-31 13:22:10 +1000

    Barbecue updates

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -1,9 +1,11 @@
+-- This is a VS Code like winbar that uses nvim-navic in order to get LSP context from your language server.
 return {
   'utilyre/barbecue.nvim',
   name = 'barbecue',
   version = '*',
   dependencies = {
     'SmiteshP/nvim-navic',
+    'catppuccin/nvim', -- barbecue needs to load after colorscheme
     'nvim-tree/nvim-web-devicons', -- optional dependency
   },
   opts = {
