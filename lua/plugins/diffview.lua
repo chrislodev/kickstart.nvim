327d9b9b85eb850117aa16b4f3a915e514ffe7ee
Chris Lo <hello@chrislo.com.au>
2025-06-04 18:01:09 +1000

    Diffview more keymaps

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -2,14 +2,24 @@ return {
   'sindrets/diffview.nvim',
   keys = {
     {
+      '<leader>gdf',
+      '<cmd>DiffviewFileHistory<cr>',
+      desc = 'Diffview file history',
+    },
+    {
       '<leader>gdo',
       '<cmd>DiffviewOpen<cr>',
-      desc = 'Open diffview',
+      desc = 'Diffview open',
     },
     {
       '<leader>gdc',
       '<cmd>DiffviewClose<cr>',
-      desc = 'Close diffview',
+      desc = 'Diffview close',
+    },
+    {
+      '<leader>gdr',
+      '<cmd>DiffviewRefresh<cr>',
+      desc = 'Diffview refresh',
     },
   },
 }
