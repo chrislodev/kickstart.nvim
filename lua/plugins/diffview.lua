614cf036fe51f78d1993e7ff99bc266bbb8919ff
Chris Lo <hello@chrislo.com.au>
2025-06-13 08:31:21 +1000

    diffview - keymaps

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -9,14 +9,30 @@ return {
   },
   keys = {
     {
-      '<leader>gdf',
+      '<leader>gdfa',
       '<cmd>DiffviewFileHistory<cr>',
-      desc = 'Diffview file history',
+      desc = 'Diffview file history (all files)',
     },
+    {
+      '<leader>gdfc',
+      '<cmd>DiffviewFileHistory --follow %<cr>',
+      desc = 'Diffview file history (current file)',
+    },
+    -- This hung neovim!
+    -- {
+    --   '<leader>gdfl',
+    --   '<cmd>.DiffviewFileHistory --follow<CR>',
+    --   desc = 'Diffview file history (current line)',
+    -- },
     {
       '<leader>gdo',
       '<cmd>DiffviewOpen<cr>',
       desc = 'Diffview open',
+    },
+    {
+      '<leader>gdm',
+      '<cmd>DiffviewOpen master<cr>',
+      desc = 'Diffview open (master)',
     },
     {
       '<leader>gdc',
